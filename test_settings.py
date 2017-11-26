372145ea11f962153cbf2e224d90181b6135a970
Cameron Curry <cameroncurry@users.noreply.github.com>
2017-11-26 11:37:03 +0000

    add urls and installed apps to test settings

--- test_settings.py
+++ test_settings.py
@@ -1,9 +1,12 @@
 SECRET_KEY = 'fake-key'
 INSTALLED_APPS = [
+    'rest_framework',
     'nabla_core',
     'nabla_api',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
 ]
-
+ROOT_URLCONF = 'nabla_api.urls'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
